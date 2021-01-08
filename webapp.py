bb6e33b2e10b09fd02892b3d82c971b721f40435
ABTCoder <amalbenson98@gmail.com>
2021-01-08 12:12:01 +0100

    Test aggiunta nodi al database

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ migrate = Migrate(app, db)
 
 engine = DefaultEngine()
 
-import models
+from models import User, Place, RedNode
 
 @app.route('/')
 def index():
@@ -35,6 +35,28 @@ def view_prob():
 
 @app.route('/view_all', methods=['GET'])
 def view_all():
+    # TEST
+    places = Place.query.all()
+    rednodes = RedNode.query.all()
+    users = User.query.all()
+    for u in users:
+        db.session.delete(u)
+    for p in places:
+        db.session.delete(p)
+    for r in rednodes:
+        db.session.delete(r)
+
+    u = User(cf="DAUIDAIWUDH", username="Test", positive=False)
+    db.session.add(u)
+    u = User.query.get(1)
+    p = Place(id=u.id, start=4000000, lat=5555555, long=3333333, finish=6000000, placeId='"Roma"')
+    db.session.add(p)
+    r = RedNode(prob=0.6, start=4000001, lat=5555555, long=3333333, finish=6000001, placeId='"Roma"')
+    db.session.add(r)
+
+    g = Place.query.get((1, 4000000))
+    print(g)
+
     query = "infect(_)"
     r = find_user_prob(query, engine)
     items = []
