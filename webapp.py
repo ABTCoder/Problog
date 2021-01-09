514769e76c13f4ecab84635e8be02307e589fcfd
alessandromanilii <alessandromanilii@gmail.com>
2021-01-09 09:59:51 +0100

    changed import package name

--- webapp.py
+++ webapp.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, flash, redirect
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
 
 from problog.engine import DefaultEngine
-from custom_predicates import find_user_prob, main_parser
 
-
+from external_functions import find_user_prob, main_parser
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,8 +16,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 engine = DefaultEngine()
-
-from models import User, Place, RedNode
 
 @app.route('/')
 def index():
@@ -35,31 +33,6 @@ def view_prob():
 
 @app.route('/view_all', methods=['GET'])
 def view_all():
-    # TEST
-    places = Place.query.all()
-    rednodes = RedNode.query.all()
-    users = User.query.all()
-    for u in users:
-        db.session.delete(u)
-    for p in places:
-        db.session.delete(p)
-    for r in rednodes:
-        db.session.delete(r)
-
-    u = User(cf="DAUIDAIWUDH", username="Test", positive=False)
-    db.session.add(u)
-    u = User.query.get(1)
-    p = Place(id=u.id, start=4000000, lat=5555555, long=3333333, finish=6000000, placeId='"Roma"')
-    p2 = Place(id=u.id, start=4000001, lat=5555555, long=3333333, finish=6000000, placeId='"Roma"')
-    db.session.add(p)
-    db.session.add(p2)
-    r = RedNode(prob=0.6, start=4000001, lat=5555555, long=3333333, finish=6000001, placeId='"Roma"')
-    db.session.add(r)
-    db.session.commit()
-
-    g = Place.query.get((1, 4000000))
-    print(g)
-
     query = "infect(_)"
     r = find_user_prob(query, engine)
     items = []
@@ -68,7 +41,6 @@ def view_all():
         end = ")"
         result = str(key)[len(start):-len(end)]
         items.append((result, value))
-
     return render_template("view_all.html", items=items)
 
 
