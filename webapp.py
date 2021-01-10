26b98384396ca3483713b14009fd742c8440dbf9
alessandromanilii <alessandromanilii@gmail.com>
2021-01-10 01:04:20 +0100

    added new methods for login, logout, register management

--- webapp.py
+++ webapp.py
@@ -1,29 +1,88 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, url_for
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 
 from problog.engine import DefaultEngine
 
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 ALLOWED_EXTENSIONS = {'json'}
+
+# Flask - DB initialization
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Flask - Login initialization
+login = LoginManager(app)
+login.login_view = 'login'  # Flask-Login needs to know what is the view function that handles logins
 
 engine = DefaultEngine()
 
 import external_functions as ef
 
+import db_functions as df
+
+from models import User
+
+import forms
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegistrationForm()
+    # validate_on_submit() method is going to return False in case the function skips the if
+    # statement and goes directly to render the template in the last line of the function
+    flash('Registrazione user')
+    if request.method == "POST" and form.validate_on_submit():
+        user = User(username=form.username.data)  # , email=form.email.data)
+        user.set_password(form.password.data)
+        df.add_user(user)  # external function for db population
+        flash('Congratulazioni, hai un account!')
+        return redirect(url_for('login'))
+        # render_template() takes a template filename and a variable list of template arguments and returns
+        # the same template, but with all the placeholders in it replaced with actual values.
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    flash('login user')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None :
+            return render_template('login.html', title='access', form=form, username_err="Invalid username")
+        if not user.check_password(form.password.data):
+            return render_template('login.html', title='access', form=form, password_err="Invalid password")
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='access', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/')
+@login_required
 def index():
+    print(current_user.is_authenticated)
     return render_template("index.html")
 
 
 @app.route('/view', methods=['POST'])
+@login_required
 def view_prob():
     id = request.form['id']
     query = 'infect("' + id + '")'
@@ -33,7 +92,9 @@ def view_prob():
 
 
 @app.route('/view_all', methods=['GET'])
+@login_required
 def view_all():
+    logout_user()
     query = "infect(_)"
     r = ef.find_user_prob(query, engine)
     items = []
@@ -46,6 +107,7 @@ def view_all():
 
 
 @app.route('/view_nodes', methods=['GET'])
+@login_required
 def view_nodes():
     places = ef.get_places()
     return render_template("view_nodes.html", places=places)
@@ -57,6 +119,7 @@ def allowed_file(filename):
 
 
 @app.route('/load_takeout', methods=['POST'])
+@login_required
 def load_takeout():
     if 'file' not in request.files:
         flash('No file part')
