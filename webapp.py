441a7ec2bbd864e2937c379f0f1830baccdd7add
alessandromanilii <alessandromanilii@gmail.com>
2021-01-26 10:51:35 +0100

    added check for 30 days expire date

--- webapp.py
+++ webapp.py
@@ -7,8 +7,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
 
+import time
 
 app = Flask(__name__)
 
@@ -24,21 +24,27 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'  # Flask-Login needs to know what is the view function that handles logins
 
-from routes import admin, home, user, health_worker
+from routes import admin, health_worker, home, user
 
-
+import models as m
 def clean_all_old_nodes():
-    with open("test.txt", 'a') as f:
-        date = datetime.now()
-        prev = datetime.fromtimestamp(date.timestamp() - 2592000)
-        f.write(str(prev)+'\n')
+    expire_limit = (time.time() - 2592000)*1000 # data corrente meno 30 giorni in millisecondi
+    for place in m.Place.query.all():
+        if place.finish < expire_limit:
+            print("nodi verdi")
+            db.session.delete(place)
+    for rnode in m.RedNode.query.all():
+        if rnode.finish < expire_limit:
+            print("nodi rossi")
+            db.session.delete(rnode)
+    db.session.commit()
 
 
 # Scheduler per la pulizia dei nodi più vecchi di 30 giorni
 scheduler = BackgroundScheduler()
 
 if os.environ.get("WERKZEUG_RUN_MAIN") == "true":  # Serve in development che altrimenti viene eseguito 2 volte di fila
-    job = scheduler.add_job(clean_all_old_nodes, 'interval', seconds=20)
+    job = scheduler.add_job(clean_all_old_nodes, 'interval', seconds=5)
 
 scheduler.start()
 
